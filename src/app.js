36e46d8ecbb6995029900a2a8e3004082da25bb4
Henrique Nogueira <henriquenogueira@henriques-mbp.home>
2021-01-18 12:17:41 +0000

    solving babel issues

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const template = React.createElement('p',{},'testing123')
+const template = <p>THIS IS JSX FROM WEBPACK</p>
 ReactDom.render (template,document.getElementById('app'))
