f1336b945b078fcadebb126cf235e3efcc18d3b8
Henrique Nogueira <henriquenogueira@henriques-mbp.home>
2021-01-25 15:26:25 +0000

    styled react-modal and mobile friendly behaviour

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -10,10 +10,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearOption}
         contentLabel="Selected Content"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3> Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearOption}>all right!</button>
+        <h3 className="modal__title"> Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearOption}>all right!</button>
     </Modal>
 )
 export default OptionModal
